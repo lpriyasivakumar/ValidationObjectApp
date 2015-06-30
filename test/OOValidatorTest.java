e28b9ddbdec4ca02f5205d8fb379203ed821b0bc
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-06-29 21:05:13 -0400

    Refactored code

---
+++ test/OOValidatorTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class OOValidatorTest {
+
+	@Test
+	public void testOOValidator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIntWithinRange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDoubleWithinRange() {
+		fail("Not yet implemented");
+	}
+
+}
